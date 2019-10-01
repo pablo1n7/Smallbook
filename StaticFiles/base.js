831ff25e193f217588c8838a17618ee767289aec
Bruno <pazosbruno@gmail.com>
2019-10-01 11:56:50 -0300

    added keyboard events to handle slide navigation (left->previous, right->next)

--- StaticFiles/base.js
+++ StaticFiles/base.js
@@ -1,6 +1,26 @@
 function format(){
-    		var b=document.getElementsByTagName("pre");
-    		for (i=0;b.length;i++) {
-    			hljs.highlightBlock(b[i],false);
-    		}
-		}
+  let b = document.getElementsByTagName("pre");
+  for (let i = 0; b.length; i++) {
+    hljs.highlightBlock(b[i],false);
+  }
+}
+
+function keyEvent(event) {
+  const key = event.keyCode || event.which
+  const arrow_left = 37
+  const arrow_right = 39
+  let slideCount = document.querySelector('.Control').innerText.split("/")
+  let current = Number(slideCount[0].replace('<<','').trim())
+  let last = Number(slideCount[1].replace('>>','').trim())
+  let previous = ((current - 1) == 0) ? last : (current - 1)
+  let next = ((current + 1) > last) ? 1 : (current + 1)
+
+  if (key == arrow_left) {
+    window.location.href = `${previous}.html`
+  }
+  if (key == arrow_right) {
+    window.location.href = `${next}.html`
+  }
+}
+
+document.addEventListener("keydown", keyEvent, false);
